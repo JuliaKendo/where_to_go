4f3b2d3bc3e61773ee55861d235ce064aca42566
Julia Kendo <ya1814@yandex.ru>
2021-03-13 16:33:42 +0300

    add Places model

--- places/models.py
+++ places/models.py
@@ -1,5 +1,10 @@
 from django.db import models
+from django.db.models.fields import CharField
 
 
 # Create your models here.
-print("hello!")
+class Places(models.Model):
+    title = CharField(max_length=150, verbose_name='place')
+
+    def __str__(self):
+        return self.title
