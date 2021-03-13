5d502732ac8284ee89e927ba6ebe17aec596fd84
Julia Kendo <ya1814@yandex.ru>
2021-03-13 17:22:08 +0300

    add model for images

--- places/admin.py
+++ places/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Places
+from .models import Place, ImagesPlace
 
 
 # Register your models here.
-admin.site.register(Places)
+admin.site.register(Place)
+admin.site.register(ImagesPlace)
--- places/models.py
+++ places/models.py
@@ -1,10 +1,22 @@
 from django.db import models
-from django.db.models.fields import CharField
 
 
 # Create your models here.
-class Places(models.Model):
-    title = CharField(max_length=150, verbose_name='place')
+class Place(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Место')
 
     def __str__(self):
         return self.title
+
+
+class ImagesPlace(models.Model):
+    place = models.ForeignKey(
+        Place, on_delete=models.CASCADE, verbose_name='Место'
+    )
+    image = models.ImageField(
+        upload_to='', null=True, blank=True, verbose_name='Изображение'
+    )
+    index_number = models.IntegerField(default=1, verbose_name='№ п/п')
+
+    def __str__(self):
+        return f'{self.index_number}. {self.place}'
