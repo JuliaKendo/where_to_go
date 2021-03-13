61941f0f3a3444bc7878d0e3b318e9558f510ff9
Julia Kendo <ya1814@yandex.ru>
2021-03-13 23:49:37 +0300

    add new fields to Place model

--- places/models.py
+++ places/models.py
@@ -4,6 +4,15 @@ from django.db import models
 # Create your models here.
 class Place(models.Model):
     title = models.CharField(max_length=150, verbose_name='Место')
+    title_short = models.CharField(
+        max_length=50, blank=True, verbose_name='Сокращенное наименование'
+    )
+    description_long = models.TextField(blank=True, verbose_name='Описание')
+    description_short = models.TextField(
+        blank=True, verbose_name='Сокращенное описание'
+    )
+    lng = models.FloatField(verbose_name='Долгота', default=0.0, blank=True)
+    lat = models.FloatField(verbose_name='Широта', default=0.0, blank=True)
 
     def __str__(self):
         return self.title
