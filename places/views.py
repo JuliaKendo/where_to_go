a9281d17832f87e41d54bc13db96f2da9814f539
Julia Kendo <ya1814@yandex.ru>
2021-03-14 15:39:40 +0300

    create json api endpoint

--- places/views.py
+++ places/views.py
@@ -1,48 +1,51 @@
-from django.shortcuts import render
-from .models import Place
+from django.http import JsonResponse
+from django.shortcuts import (
+    render, get_object_or_404, get_list_or_404
+)
+from .models import Place, ImagesPlace
 
 
-def get_places():
-    features = []
-    places = Place.objects.all()
-    for place in places:
-        features.append({
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [place.lng, place.lat]
-            },
-            "properties": {
-                "title": place.title_short,
-                "placeId": place.id,
-                "details": {
-                    "title": place.title,
-                    "imgs": [
-                        "https://raw.githubusercontent.com/devmanorg/where-to-go-places/master/media/4f793576c79c1cbe68b73800ae06f06f.jpg",
-                        "https://raw.githubusercontent.com/devmanorg/where-to-go-places/master/media/7a7631bab8af3e340993a6fb1ded3e73.jpg",
-                        "https://raw.githubusercontent.com/devmanorg/where-to-go-places/master/media/a55cbc706d764c1764dfccf832d50541.jpg",
-                        "https://raw.githubusercontent.com/devmanorg/where-to-go-places/master/media/65153b5c595345713f812d1329457b54.jpg",
-                        "https://raw.githubusercontent.com/devmanorg/where-to-go-places/master/media/0a79676b3d5e3b394717b4bf2e610a57.jpg",
-                        "https://raw.githubusercontent.com/devmanorg/where-to-go-places/master/media/1e27f507cb72e76b604adbe5e7b5f315.jpg"
-                    ],
-                    "description_short": place.description_short,
-                    "description_long": place.description_long,
-                    "coordinates": {
-                        "lng": place.lng,
-                        "lat": place.lat
-                    }
+def get_features(place):
+    images = get_list_or_404(ImagesPlace, place=place)
+    return {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [place.lng, place.lat]
+        },
+        "properties": {
+            "title": place.title_short,
+            "placeId": place.id,
+            "details": {
+                "title": place.title,
+                "imgs": [item.image.url for item in images],
+                "description_short": place.description_short,
+                "description_long": place.description_long,
+                "coordinates": {
+                    "lng": place.lng,
+                    "lat": place.lat
                 }
             }
-        })
-
-    return {
-        "type": "FeatureCollection",
-        "features": features
+        }
     }
 
 
 def index(request):
+    places = Place.objects.all()
     return render(
         request, 'index.html',
-        {'places': get_places()}
+        {
+            'places': {
+                "type": "FeatureCollection",
+                "features": [get_features(place) for place in places]
+            }
+        }
+    )
+
+
+def place_detail_view(request, place_id):
+    place = get_object_or_404(Place, id=place_id)
+    return JsonResponse(
+        get_features(place),
+        json_dumps_params={'ensure_ascii': False, 'indent': 2}
     )
