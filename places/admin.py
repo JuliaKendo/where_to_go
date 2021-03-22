4ebf730a503b7685a4f46ffbc640c2a48ca66a76
Julia Kendo <ya1814@yandex.ru>
2021-03-22 19:59:38 +0300

    handle miss image in the preview

--- places/admin.py
+++ places/admin.py
@@ -6,11 +6,14 @@ from .models import Place, ImagesPlace
 
 class ImagesPlaceInLine(SortableInlineAdminMixin, admin.TabularInline):
     model = ImagesPlace
+    extra = 0
     readonly_fields = ('preview_field',)
     fields = (('place', 'image'), 'preview_field', 'index_number')
 
     def preview_field(self, instance):
-        return format_html('<img src={} height=100px />', instance.image.url)
+        if instance.image:
+            return format_html('<img src={} height=100px />', instance.image.url)
+        return 'Здесь будет превью, когда вы выберете файл'
 
 
 class PlaceAdmin(admin.ModelAdmin):
